4d1671925717360d2d62749dfbc369ec2d5d847c
Louie Weng <louieweng@outlook.com>
2023-10-23 00:42:11 -0400

    Update lazy lock

--- nvim/lua/plugins/markdown_preview.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  event = "VeryLazy",
-  build = function() vim.fn["mkdp#util#install"]() end,
-}
