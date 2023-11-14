a18a063565da80ec7d6f4c0c2d554a23b9429aa9
Louie Weng <louieweng@outlook.com>
2023-11-14 00:45:28 -0500

    Fix logging for tmux tpm hook

--- tmux/hooks/after.sh
+++ tmux/hooks/after.sh
@@ -6,11 +6,11 @@ if [[ -z $SOURCE_PATH || -z $TARGET_PATH ]]; then
   exit 0
 fi
 
-if [[ ! -d "$SOURCE_PATH/plugins/tpm" ]]; then
-  echo "-> Cloning TPM into $SOURCE_PATH/plugins/tpm"
-  git clone https://github.com/tmux-plugins/tpm "$SOURCE_PATH/plugins/tpm"
+if [[ ! -d "${SOURCE_PATH}plugins/tpm" ]]; then
+  echo "-> Cloning TPM into ${SOURCE_PATH}plugins/tpm"
+  git clone https://github.com/tmux-plugins/tpm "${SOURCE_PATH}plugins/tpm"
 else
-  echo "-> TPM source already exists in [$SOURCE_PATH/plugins/tpm]"
+  echo "-> TPM source already exists in [${SOURCE_PATH}plugins/tpm]"
 fi
 
   
