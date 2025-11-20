cda6c49f76fa38a578c73a920e71c7f27faac5a3
lourw <56288712+lourw@users.noreply.github.com>
2025-11-20 00:04:11 -0800

    chore(nvim): autoregister lsp configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,12 @@ require('configs.keymaps')
 require('configs.settings')
 require('configs.lazy_config')
 
-vim.lsp.enable("zls")
+-- Auto-load all LSP configs
+local lsp_configs = {}
+
+for _, f in pairs(vim.api.nvim_get_runtime_file('lsp/*.lua', true)) do
+  local server_name = vim.fn.fnamemodify(f, ':t:r')
+  table.insert(lsp_configs, server_name)
+end
+
+vim.lsp.enable(lsp_configs)
